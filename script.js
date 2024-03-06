619d4187d576f55ce2e444cbaba36963f2b1ccc4
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-06 21:40:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code")
+codes[0].focus();
+
+
+codes.forEach((code,index)=>{
+
+
+code.addEventListener("keydown",(eventDetails) =>{
+if(eventDetails.key>=0 && eventDetails.key<=9){
+    codes[index].value = "";
+    setTimeout(()=>{codes[index+1].focus()},10)
+}
+else if(eventDetails.key == "Backspace"){
+    setTimeout(()=>{codes[index-1].focus()},10)
+}
+})
+})
