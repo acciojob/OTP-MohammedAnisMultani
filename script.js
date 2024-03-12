2b87e972e28c53e3cf00c28a884d9603de8752f7
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-12 22:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,20 +16,21 @@
 // }
 // })
 
+const codes = document.querySelectorAll(".code");
+codes[0].focus();
 
-const codes = document.querySelectorAll(".code")
-codes[0].focus()
-
-codes.forEach((code,i)=>{
-    code.addEventListener("keydown", (e)=>{
-        if(e.key>=0 && e.key<=9){
-            codes[i].value == "";
-            setTimeout(()=>codes[i+1].focus(), 10)
+codes.forEach((code, i) => {
+    code.addEventListener("keydown", (e) => {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[i].value = "";
+            if (codes[i + 1]) {
+                setTimeout(() => codes[i + 1].focus(), 10);
+            }
+        } else if (e.key === "Backspace") {
+            if (codes[i - 1]) {
+                setTimeout(() => codes[i - 1].focus(), 10);
+            }
         }
-        else if(e.key == "Backspace"){
-            setTimeout(()=>codes[i-1].focus(), 10)
-        }
-    })
-
-})
+    });
+});
 // })
