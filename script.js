d8477a9ae735146dc81a280ac0f4ff75a1885890
MohammedAnisMultani <147980347+MohammedAnisMultani@users.noreply.github.com>
2024-03-12 22:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-//your JS code here. If required.
+// //your JS code here. If required.
+// const codes = document.querySelectorAll(".code")
+// codes[0].focus();
+
+
+// codes.forEach((code,index)=>{
+
+
+// code.addEventListener("keydown",(eventDetails) =>{
+// if(eventDetails.key>=0 && eventDetails.key<=9){
+//     codes[index].value = "";
+//     setTimeout(()=>{codes[index+1].focus()},10)
+// }
+// else if(eventDetails.key == "Backspace"){
+//     setTimeout(()=>{codes[index-1].focus()},10)
+// }
+// })
+
+
 const codes = document.querySelectorAll(".code")
-codes[0].focus();
+codes[0].focus()
 
+codes.forEach((code,i)=>{
+    code.addEventListener("keydown", (e)=>{
+        if(e.key>=0 && e.key<=9){
+            codes[i].value == "";
+            setTimeout(()=>codes[i+1].focus(), 10)
+        }
+        else if(e.key == "Backspace"){
+            setTimeout(()=>codes[i-1].focus(), 10)
+        }
+    })
 
-codes.forEach((code,index)=>{
-
-
-code.addEventListener("keydown",(eventDetails) =>{
-if(eventDetails.key>=0 && eventDetails.key<=9){
-    codes[index].value = "";
-    setTimeout(()=>{codes[index+1].focus()},10)
-}
-else if(eventDetails.key == "Backspace"){
-    setTimeout(()=>{codes[index-1].focus()},10)
-}
 })
-})
+// })
